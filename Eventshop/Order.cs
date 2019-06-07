fb8b795be38dc97615e416424ce12367d1c308a0
Luca Bucher <lucbu01@bluewin.ch>
2019-06-07 14:01:20 +0200

    initialize help classes

---
+++ Eventshop/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventshop {
+  class Order {
+    public int ID { get; set; }
+    public int CountPerson { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string Address { get; set; }
+    public string PostalCodeAndPlace { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public Event Event { get; set; }
+  }
+}
